8a40cdfd96f35b31c2a7439579b8981094878192
Vishnu Vardhan <vishnu.vardhan@smartbots.ai>
2025-11-25 02:54:56 +0530

    updated api url

--- src/services/rag-api.service.ts
+++ src/services/rag-api.service.ts
@@ -6,7 +6,7 @@ import { catchError, Observable, throwError } from 'rxjs';
   providedIn: 'root'
 })
 export class RagApiService {
-  private apiUrl = 'http://127.0.0.1:5000/api';
+  private apiUrl = 'http://127.0.0.1:8000/api';
 
   constructor(private http: HttpClient) { }
 
